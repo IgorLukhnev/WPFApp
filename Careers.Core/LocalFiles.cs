ffecf82e8cfe84c0aea1f87fe84d8d958776c479
Игорь Лухнев <idlukhnev@edu.hse.ru>
2020-10-08 17:09:51 +0300

    Added LoadUniversities method

--- Careers.Core/LocalFiles.cs
+++ Careers.Core/LocalFiles.cs
@@ -1,6 +1,7 @@
 ﻿using Careers.Core.Model;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace Careers.Core {
@@ -9,7 +10,34 @@ namespace Careers.Core {
         public List<University> Universities { get; set; }
         public LocalFiles()
         {
-            UniversitiesLocation = "C:\\Users\\Пользователь\\source\repos\\CareerManager\\Careers.Core\bin\\Debug";
+            UniversitiesLocation = ".\\Careers.Core\bin\\Debug";
+        }
+        public void LoadUniversities()
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader(UniversitiesLocation))
+                {
+                    string line;
+                    Universities = new List<University>();
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        var info = line.Split(',');
+                        University newUniversity = new University
+                        {
+                            Country = info[0],
+                            Name = info[1]
+                        };
+                        Universities.Add(newUniversity);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
